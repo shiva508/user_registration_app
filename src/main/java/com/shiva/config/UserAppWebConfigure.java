db111e317fe5a39733e187a4930dabcd139b57e9
shiva508 <dasarishiva1@gmail.com>
2019-11-21 17:04:47 +0530

    I18

--- src/main/java/com/shiva/config/UserAppWebConfigure.java
+++ src/main/java/com/shiva/config/UserAppWebConfigure.java
@@ -1,5 +1,7 @@
 package com.shiva.config;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.MessageSource;
@@ -12,10 +14,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -30,11 +34,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.shiva" })
-@PropertySource(value= {"classpath:application.properties"})
+@PropertySource(value = { "classpath:application.properties" })
 @EnableSwagger2
 public class UserAppWebConfigure extends WebMvcConfigurerAdapter {
 	@Autowired
-	private Environment environment; 
+	private Environment environment;
+
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -46,43 +51,47 @@ public class UserAppWebConfigure extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String[] pathPatterns = { "/components/**", "/css/**", "/js/**"};
-		String[] resourceLocations = {"/components/", "/css/", "/js/"};
+		String[] pathPatterns = { "/components/**", "/css/**", "/js/**" };
+		String[] resourceLocations = { "/components/", "/css/", "/js/" };
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 		registry.addResourceHandler(pathPatterns).addResourceLocations(resourceLocations);
 	}
+
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-	@Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
-    }
+
 	public HttpEntity<String> httpEntity() {
-		HttpHeaders handler=new HttpHeaders();
+		HttpHeaders handler = new HttpHeaders();
 		handler.set("Authorization", environment.getProperty("ASANA_ACCESS_TOKEN"));
 		HttpEntity<String> entity = new HttpEntity<String>("body", handler);
 		return entity;
-		
+
+	}
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.shiva.controller")).paths(PathSelectors.any()).build();
+	}
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("USER REGISTARION APP").description("DATA").version("1.0.0").build();
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(Locale.ENGLISH);
+		return resolver;
 	}
 	@Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(getApiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.shiva.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-	private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("USER REGISTARION APP")
-                .description("DATA")
-                .version("1.0.0")
-                .build();
-    }
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source=new ResourceBundleMessageSource();
+		source.setBasename("message");
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
+	}
 }
--- src/main/java/com/shiva/controller/LoginRestController.java
+++ src/main/java/com/shiva/controller/LoginRestController.java
@@ -1,12 +1,19 @@
 package com.shiva.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginRestController {
-	@GetMapping
-	public String one() {
-		return null;
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	@GetMapping("/i18")
+	public String one(@RequestHeader("Accept-Language")String locale) {
+		return messageSource.getMessage("hello.txt", null, new Locale(locale));
 	}
 }
